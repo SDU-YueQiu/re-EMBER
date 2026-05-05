972d908fb806e708390b9fec82b6fa118d41015f
Yueq2003 <iop18653346184@hotmail.com>
2026-05-05 19:35:47 +0800

    添加可选Tracy性能插桩开关

---
+++ src/core/perf_tracing.h
@@ -0,0 +1,44 @@
+/**
+ * @file perf_tracing.h
+ * @brief 定义可选 Tracy 性能插桩的本地包装宏。
+ */
+#pragma once
+
+#if defined(REEMBER_ENABLE_TRACY)
+
+#include <tracy/Tracy.hpp>
+
+/**
+ * @brief 标记当前作用域为一个 Tracy zone。
+ */
+#define REEMBER_PROFILE_ZONE(nameLiteral) ZoneScopedN(nameLiteral)
+
+/**
+ * @brief 按函数名标记当前作用域为一个 Tracy zone。
+ */
+#define REEMBER_PROFILE_FUNCTION() ZoneScoped
+
+/**
+ * @brief 向 Tracy 记录一个数值序列。
+ */
+#define REEMBER_PROFILE_VALUE(nameLiteral, value) TracyPlot(nameLiteral, static_cast<double>(value))
+
+/**
+ * @brief 向 Tracy 当前捕获流写入一段消息。
+ */
+#define REEMBER_PROFILE_MESSAGE(text, size) TracyMessage((text), (size))
+
+/**
+ * @brief 标记一个 Tracy 帧边界。
+ */
+#define REEMBER_PROFILE_FRAME(nameLiteral) FrameMarkNamed(nameLiteral)
+
+#else
+
+#define REEMBER_PROFILE_ZONE(nameLiteral) ((void)0)
+#define REEMBER_PROFILE_FUNCTION() ((void)0)
+#define REEMBER_PROFILE_VALUE(nameLiteral, value) ((void)0)
+#define REEMBER_PROFILE_MESSAGE(text, size) ((void)0)
+#define REEMBER_PROFILE_FRAME(nameLiteral) ((void)0)
+
+#endif
