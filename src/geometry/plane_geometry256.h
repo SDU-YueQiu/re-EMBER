85cf538daf5cd7bda90b31ef997aafe70f6a3ecf
Yueq2003 <iop18653346184@hotmail.com>
2026-05-21 02:28:26 +0800

    Revert "优化BitInt的GCD和整除路径"
    This reverts commit 011d16cca98c3e6d3079445192123a4ea7eaad61.

--- src/geometry/plane_geometry256.h
+++ src/geometry/plane_geometry256.h
@@ -17,10 +17,10 @@ inline void reducePlaneCoefficients(Integer &a, Integer &b, Integer &c, Integer 
     if (divisor <= 1)
         return;
 
-    a = detail::divideExactByPositive(a, divisor);
-    b = detail::divideExactByPositive(b, divisor);
-    c = detail::divideExactByPositive(c, divisor);
-    d = detail::divideExactByPositive(d, divisor);
+    a /= divisor;
+    b /= divisor;
+    c /= divisor;
+    d /= divisor;
 }
 
 struct Plane3i
